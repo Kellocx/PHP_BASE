5b48f1836a80b3cc389147dd7e6cdabddde5e41b
Kellocx <marialessandro79@gmail.com>
2025-11-06 17:43:43 +0100

    2

--- tabellina/index.php
+++ tabellina/index.php
@@ -1,40 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabellina</title>
-</head>
+if (isset($_POST['numero'])) {
 
-<body>
-    <form action="" method="get">
+    $num = $_POST['numero'];
 
-        Inserisci numero : <input type="number" name="numero">
-
-        <input type="submit">
-        <input type="submit" value="MOSTRA TABELLINA">
-
-
-
-    </form>
-    <?php
-    if(isset ($_POST["numero"])){
-        $num = $_POST['numero'];
-   
-    
-
-    // Stampo la tabellina del numero inserito
-    echo "Tabellina del numero $numeroInserito: ";
     for ($i = 0; $i <= 10; $i++) {
-      
-        echo "$num x $i = ". ($num * $i) . "br";
-        }
+
+        echo "$num x $i = " . ($num * $i) . "<br>";
     }
-    ?>
+}
+?>
 
 
 
-</body>
+<form action="" method="post">
 
-</html>
+    Inserisci un numero :
+
+    <input type="numer" name="numero">
+    <input type="submit" value="Mostra atabellina">
+
+</form>
