cd5986b38513317aebde846da242b84e47090b78
Kellocx <marialessandro79@gmail.com>
2025-11-06 15:50:30 +0100

    refactoring

--- calcolomedia/index.php
+++ calcolomedia/index.php
@@ -1,14 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Media</title>
-</head>
-
-<body>
-    <!DOCTYPE html>
     <html lang="it">
 
     <head>
@@ -19,12 +9,13 @@
 
     <body>
         <h1>Inserisci 5 numeri</h1>
+        <!-- Form -->
         <form method="post">
             <?php for ($i = 1; $i <= 5; $i++): ?>
                 Inserisci 5 Numeri 
                 <input type="number" name="numeri[]" required>
                 <br>
-            <?php endfor; ?>
+            <?php endfor; ?><!-- -->
             <input type="submit" value="Calcola Media">
         </form>
 
@@ -36,9 +27,7 @@
             echo "<h2>La media dei numeri inseriti è: " . number_format($media, 2) . "</h2>";
         
         ?>
-    </body>
 
-    </html>
 
 
 </body>
