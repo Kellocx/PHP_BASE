616a3e839670ee63a805860ea87a743d3e21ac5e
Kellocx <marialessandro79@gmail.com>
2025-11-06 18:01:48 +0100

    refactoring

--- contaVocali/index.php
+++ contaVocali/index.php
@@ -1,32 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ContaVocali</title>
-</head>
-<body>
-    <?php
+<?php
 
-    if(isset($_POST['frase'])){
-        $frase = strtolower($_POST['frase']);
 
-        $vocali = ["a", "e", "i", "o", "u"];// dichiaro le vocali
+// voglio contare quante vocali ci sono in una frase (stringa)
 
-        $contaVocali = 0;
+if (isset($_POST['frase'])) {
 
-        for($i = 0; $i <($frase); $i++){
-            if(in_array($frase[$i], $vocali)){
-                $contaVocali++;
-            }
+
+    $frase = strtolower($_POST['frase']); // rendo minuscolo con strtolower
+
+    //definisco le vocali al pc
+    $vocali = ["a", "e", "i", "o", "u"];
+
+    //inizializzo contenitore counter per avere il numero di vocali
+    $contaNumeroVocali = 0;
+
+    //scorre ogni carattere della frase
+    //strlen -> è la lunghezza dell array ( .lenght di js )
+    for ($i = 0; $i < strlen($frase); $i++) {
+
+        //verifica se un valore è presente nell array
+        if (in_array($frase[$i], $vocali)) {  //prendo $i della frase e confronto con $vocali
+
+            $contaNumeroVocali++; //incremento il numero di vocali trovate
         }
-        echo "<p> La frase contine $contavocali vocali";
     }
 
+    echo " <p>La frase contiene $contaNumeroVocali vocali</p>";
+}
+?>
 
 
 
-    ?>
-    
-</body>
-</html>
+<form action="" method="post">
+
+    Inserisci una frase : <input type="text" name="frase">
+    <input type="submit" value="conta vocali">
+
+
+</form>
