344fbf70df39288f8b63a4da1b5f267ac5bfc01c
Kellocx <marialessandro79@gmail.com>
2025-11-07 14:48:48 +0100

    sceond commit

--- listaUtenti/index.php
+++ listaUtenti/index.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista Utenti</title>
+    <title>Rubrica Contatti</title>
 </head>
+
 <body>
-    
+    <h1>Rubrica Contatti</h1><br>
+
+    <h2>Aggiungi Contatto</h2>
+
+    <form action="" method="post">
+        Nome: <input type="text" name="name">
+        Telefono: <input type="text" name="phone">
+
+        <button type="submit" name="add">Aggiungi contatto</button>
+
+
+
+    </form>
+
+    <h2>Ricerca Contatto</h2>
+
+    <form action="" method="post">
+        Nome: <input type="text" name="serach_name">
+        
+
+        <button type="submit" name="search">Cerca</button>
+
+
+
+    </form>
+
+
+
 </body>
+
 </html>
